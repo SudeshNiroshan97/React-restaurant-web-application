f576dca30ab9888cc7c83e9ac1591964b6375b42
Sudesh  Ladusinghe <sudeshniroshan507@gmail.com>
2021-07-02 09:03:38 +0530

    feat: updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import {css} from "@emotion/core"
 import { PropagateLoader } from 'react-spinners/PropagateLoader';
 
 function App() {
+
+  const[loading,setLoading] = useState(false);
+  const override = css`
+  display:block;
+  border-color:red;
+  margin-top:20% `;
+
+  useEffect(() => {
+
+    setLoading(true)
+    setTimeout(() => {
+
+      setLoading(false)
+
+    }, 5000);
+
+
+  },[])
+
+
+
   return (
     <div className="App">
+
+      {
+
+        loading ? <PropagateLoader color={"#3d2514"} loading={loading} css={override} size={40}/>
+        :
+        <>
+        <Navbar/>
+        </>
+
+      }
      
     </div>
   );
